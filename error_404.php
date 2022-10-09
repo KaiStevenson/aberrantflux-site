2b37060779d20640a8495fdbb0310c7d06fbac32
Kai Stevenson <kai@kaistevenson.com>
2022-10-08 19:41:23 -0700

    added 404 page

---
+++ error_404.php
@@ -0,0 +1,9 @@
+<?php
+$title = "Page not found [404]";
+require($_SERVER["DOCUMENT_ROOT"] . "/head.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/header.php");
+?>
+<p>
+    The page you tried to access doesn't exist. I probably just haven't made it yet.
+</p>
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/footer.php"); ?>
