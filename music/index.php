099e22a7439a7df4bafe2949387fcd4c7cfa90f5
Kai Stevenson <kai@kaistevenson.com>
2022-10-15 15:45:33 -0700

    added more text

--- music/index.php
+++ music/index.php
@@ -5,7 +5,15 @@ require($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 ?>
 <p>
     The following links lead to music I've recorded. Anything marked with # is improv or my own 
-    composition. 
+    composition. Anything else isn't my music, but is my playing. These recordings originate from
+    one of two pianos: my 1903 Bechstein parlor grand or my Casio PX-S3000 digital piano.
+    My digital piano can obviously record itself. For my acoustic piano, I either use my phone's
+    voice memo app or my MXL990 microphone, depending on how lazy I feel.
+    Any editing is done in Audacity or Ableton, though most of my recordings are posted raw.
+</p>
+<p>
+    I've only played music since 2021, so I am very much an amateur! I enjoy some of my music
+    and hope you will too, but I don't want to imply that I am in any way an expert.
 </p>
 <ul>
     <li>
