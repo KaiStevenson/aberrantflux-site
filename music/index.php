3f2f4f8cedd2a7090929ea470edfa5cd5a76c210
Kai Stevenson <kai@kaistevenson.com>
2022-10-15 15:07:52 -0700

    updated music index

--- music/index.php
+++ music/index.php
@@ -4,7 +4,7 @@ require($_SERVER["DOCUMENT_ROOT"] . "/head.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 ?>
 <p>
-    The following links lead to music I've recorded. Anything marked with "*" is improv or my own 
+    The following links lead to music I've recorded. Anything marked with # is improv or my own 
     composition. 
 </p>
 <ul>
@@ -12,7 +12,7 @@ require($_SERVER["DOCUMENT_ROOT"] . "/header.php");
         <h3><a href="angelfish.mp3">Angelfish</a></h3>
     </li>
     <li>
-        <h3><a href="music/dne.html">09/15/22 *</a></h3>
+        <h3><a href="music/dne.html">09/15/22 - #</a></h3>
     </li>
 </ul>
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/footer.php"); ?>
