0baa2d42ced33c2a60e503b249f0011f2e5cc691
Kai Stevenson <kai@kaistevenson.com>
2022-10-29 12:12:19 -0700

    -m

--- src/about.php
+++ src/about.php
@@ -8,7 +8,7 @@ This site is hosted on a vultr VPS. It serves content through an apache webserve
 I use PHP for templating, CSS for styling, and no JavaScript at all. The server supports HTTPS using Let's Encrypt.
 If you're curious about anything here, you can email me at kai@kaistevenson.com.
 </p>
-<p>This site is a member of the <a href="https://nullring.xyz">Null Webring</a>.</p>
+<p>This site is a member of the <a href="https://nullring.xyz">Null Webring</a>, which I helped to create.</p>
 <div style="width: 100%;  text-align: center;">
     <pre class="ascii" style="display: inline-block; text-align: left;">   
                                                  ,▄▓▄,
@@ -38,5 +38,5 @@ If you're curious about anything here, you can email me at kai@kaistevenson.com.
      ╙
     </pre>
 </div>
-<h3 class="nav"><a>[Last site]</a> | <a href="https://www.nullring.xyz">[Nullring]</a> | <a>[Next site]</a></h3>
+<h3 class="nav"><a href="https://prestonpan.tech">[Last site]</a> | <a href="https://www.nullring.xyz">[Nullring]</a> | <a href="https://society.nullring.xyz">[Next site]</a></h3>
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/footer.php"); ?>
