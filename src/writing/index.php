18ad71640a5b12dc85b0093bcabbd704c8f03f0c
Kai Stevenson <kai@kaistevenson.com>
2022-10-31 12:16:19 -0700

    small tweaks to text

--- src/writing/index.php
+++ src/writing/index.php
@@ -4,16 +4,15 @@ require($_SERVER["DOCUMENT_ROOT"] . "/head.php");
 require($_SERVER["DOCUMENT_ROOT"] . "/header.php");
 ?>
 <p>
-    This is the index page for my writing. Obviously, if you follow the links below, you can read things I've
-    written.
-    Whether or not you want to do that is up to you.
+    This is the index page for my writing. Each link leads to a different thing that I've written!
+    Some of these were written for assignments, and some just because I felt like it.
 </p>
 <ul>
     <li>
-        <h3><a href="cryptography.php">Cryptography</a> /* Keep yourself safe */</h3>
+        <h3><a href="cryptography.php">Cryptography</a> /* Keep your secrets */</h3>
     </li>
     <li>
-        <h3><a href="moralrealism.php">Moral realism</a> /* (It's not real)  */</h3>
+        <h3><a href="moralrealism.php">Moral realism</a> /* Murder is fine, actualy */</h3>
     </li>
 </ul>
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/footer.php"); ?>
